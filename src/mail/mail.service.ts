4406c2cd0b1f3e180b4eb5e1fdfbb2880c51893e
Joss <133165583+IngenieroJosser@users.noreply.github.com>
2025-03-27 18:14:01 -0500

    feat(auth): sending the OTP to the email to recover the password 1.1

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -2,6 +2,7 @@
 import { Injectable } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
 import { VerifyOtpDto } from 'src/auth/dto/verifyOtp.dto';
+import { RegisterDto } from 'src/auth/dto/createUser.dto';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
@@ -26,9 +27,35 @@ export class MailService {
       to: dataMail.email,
       subject: 'Código OTP para reestablecer tu contraseña',
       text: `Tu código OTP es: ${dataMail.otp}. Este código expira en 10 minutos.`,
-      html: `<p><strong>Tu código OTP:</strong> ${dataMail.otp}</p><p>Este código expirará en 10 minutos.</p>`
-    };
-
+      html: `
+        <div style="font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 20px;">
+          <div style="max-width: 600px; margin: auto; border: 1px solid #ddd; border-radius: 5px; overflow: hidden;">
+            <div style="background-color: #008060; padding: 10px 20px; text-align: center;">
+              <!-- Reemplaza el texto por una imagen inline -->
+              <img src="cid:logo" alt="ChocóMarket" style="max-width: 150px;">
+            </div>
+            <div style="padding: 20px; color: #5A3E29;">
+              <p style="font-size: 16px;">Hola ${dataMail.email},</p>
+              <p style="font-size: 16px;">
+                Tu código OTP es: <strong style="color: #ff0000;">${dataMail.otp}</strong>
+              </p>
+              <p style="font-size: 16px;">Este código expirará en 10 minutos.</p>
+              <p style="font-size: 14px; color: #777;">Si no solicitaste este código, ignora este mensaje.</p>
+            </div>
+            <div style="background-color: #f1f1f1; padding: 10px 20px; text-align: center; font-size: 12px; color: #777;">
+              <p>&copy; ${new Date().getFullYear()} ChocóMarket. Todos los derechos reservados.</p>
+            </div>
+          </div>
+        </div>
+      `,
+      attachments: [
+        {
+          filename: 'logo.png',
+          path: 'src/img/logo.png', // ruta en tu servidor
+          cid: 'logo', // el mismo identificador que usas en el src de la imagen
+        },
+      ],
+    };        
     await this.transporter.sendMail(mailOptions);
   }
 }
